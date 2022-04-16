93585d9467b3e58066da92de587c9e0a2272069a
Nehaxfr <benji.vandeginste@gmail.com>
2022-04-16 13:00:05 +0200

    various test with route and decorator

--- Source/Server/Routes.js
+++ Source/Server/Routes.js
@@ -2,17 +2,23 @@
 
 class Routes {
 
+    /**
+     * Add all needed routes to fastify server
+     * @param {fastify} fastify - fastify server
+     */
     static initializeRoutes(fastify) {
         // test route
         Routes.addRoute(fastify, "/test", testHandler, "GET");
+        Routes.addRoute(fastify, "/testBSG", testHandlerBSG, "GET");
+        Routes.addRoute(fastify, "/testBSGCompress", testHandlerBSGCompress, "GET");
     }
 
     /**
      * Template function that can add routes to the server dynamically
      * @param {fastify} fastify - fastify server
      * @param {string} route - route to add (ex: /api/v1/user/)
-     * @param {function} handler - the function that will be executed when the route is called
-     * @param {string} method - the method that will be used to call the route (ex: GET, POST, PUT, DELETE)
+     * @param {function} handler - function that will be executed when the route is called
+     * @param {string} method - method that will be used to call the route (ex: GET, POST)
      */
     static addRoute(fastify, route, handler, method) {
         fastify.route({
@@ -41,6 +47,13 @@ function testHandler(req, reply) {
     reply.send({France: "baise ouai"});
 }
 
+function testHandlerBSG(req, reply) {
+    reply.send(reply.Res_BSG({France: "baise ouai"}));
+}
 
-module.exports.testHandler = testHandler;
+function testHandlerBSGCompress(req, reply) {
+    reply.send(reply.Res_Compress(JSON.stringify(reply.Res_BSG({France: "baise ouai"}))));
+}
+
+
 module.exports = Routes;
