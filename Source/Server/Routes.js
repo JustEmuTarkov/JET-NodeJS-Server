c403f8df8f25ca4dfc20d0ffc9bd3f1af3fbe4ef
Nehaxfr <benji.vandeginste@gmail.com>
2022-04-16 11:03:41 +0200

    addRoute() -> initialize routes "dynamically"

--- Source/Server/Routes.js
+++ Source/Server/Routes.js
@@ -1,14 +1,46 @@
-class Routes 
-{
-    constructor(Fastify)
-    {
-        Fastify.AddRoute("/", function(req, reply) { 
+
+
+class Routes {
+
+    static initializeRoutes(fastify) {
+        // test route
+        Routes.addRoute(fastify, "/test", testHandler, "GET");
+    }
+
+    /**
+     * Template function that can add routes to the server dynamically
+     * @param {fastify} fastify - fastify server
+     * @param {string} route - route to add (ex: /api/v1/user/)
+     * @param {function} handler - the function that will be executed when the route is called
+     * @param {string} method - the method that will be used to call the route (ex: GET, POST, PUT, DELETE)
+     */
+    static addRoute(fastify, route, handler, method) {
+        fastify.route({
+            method: method,
+            url: route,
+            handler: handler
+        });
+    }
+
+    /**
+     * Mao test route
+     * @param {fastify} fastify 
+     */
+    static maoTestRoute(fastify) {
+        fastify.AddRoute("/", function(req, reply) { 
             const body = req.Req_Body2Json(req);
-            console.log(body);
+            console.log("body: " + body);
             console.log(req.jsonBody);
             reply.ResponseHeader(reply, 12345);
             reply.send(reply.Res_Compress(reply.Res_BSG({ "response": "OK" })));
         }, "get&post");
     }
 }
+
+function testHandler(req, reply) {
+    reply.send({France: "baise ouai"});
+}
+
+
+module.exports.testHandler = testHandler;
 module.exports = Routes;
--- Source/index.js
+++ Source/index.js
@@ -2,27 +2,30 @@
 
 const fs = require("fs");
 const path = require("path");
+const fastify = require("./Server/Fastify.js");
+const routes = require("./Server/Routes.js");
 
 global.JET = { ExecutionPath: __dirname, UserList: [], UserDataList: [], Utils: {} }
 global.JET.Utils = require('./Core/Utils');
 
 console.log(JET.Utils);
-// const Fastify = require("./Server/Fastify.js");
-// const Routes = new (require("./Server/Routes.js"))(Fastify);
-// const Database = require("./Server/Database.js");
-// global.JET.Database = Database;
+
+routes.initializeRoutes(fastify.Server);
+console.log(routes);
+const database = require("./Server/Database.js");
+global.JET.Database = database;
 
 
-// const LoadServerProfileList = () => {
-//     const Profiles = fs.readdirSync(global.JET.ExecutionPath + "/User/Profiles");
-//     for(let profileId of Profiles){
-//         if(profileId.includes(".")) continue;
-//         global.JET.UserList.push(profileId);
-//     }
-// }
+//const LoadServerProfileList = () => {
+//    const Profiles = fs.readdirSync(global.JET.ExecutionPath + "/User/Profiles");
+//    for(let profileId of Profiles){
+//        if(profileId.includes(".")) continue;
+//        global.JET.UserList.push(profileId);
+//    }
+//}
 
 
 // //Fastify.Server.decorate("jet_db", Database);
 
 // LoadServerProfileList();
-// Fastify.StartServer();
+fastify.StartServer();
