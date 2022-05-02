e1607eb079ef7b67cb0023706a7ef1e4d23157e1
Nehaxfr <benji.vandeginste@gmail.com>
2022-05-02 17:27:31 +0200

    the stripclub is on fire

--- server/stripclub.js
+++ server/stripclub.js
@@ -9,6 +9,7 @@ class Hooker {
                 request.req.on('data', (chunk) => {
                     body.push(chunk);
                 }).on('end', () => {
+                    console.log(body.toString());
                     let data = Buffer.concat(body);
                     console.log(data.toString());
                 });
@@ -19,16 +20,16 @@ class Hooker {
                 request.req.on('data', (chunk) => {
                     body.push(chunk);
                 }).on('end', () => {
-                    console.log(body);
-                    let data = Buffer.concat(body);
-                    console.log(data);
-                    zlib.inflate(data, function(err, body){
-                        if (body !== undefined) {
-                            request.body = body !== typeof "undefined" && body !== null && body !== "" ? body.toString() : "{}";
+                    console.log(body[0]);
+                    zlib.inflate(body[0], function(err, data){
+                        console.log(data)
+                        if (data !== undefined) {
+                            request.body = data !== typeof "undefined" && data !== null && data !== "" ? data.toString() : "{}";
                         }
                     })
                 })
             }
+            
             done();
         })
 
