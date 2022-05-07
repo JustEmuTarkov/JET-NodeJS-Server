a61b61a40366765534f98b233093973404ddd7f4
IAMBUDE <93278696+IAMBUDE@users.noreply.github.com>
2022-05-07 17:31:20 +0200

    Update fastify.js
    Content parse ?fix?

--- server/fastify.js
+++ server/fastify.js
@@ -2,9 +2,10 @@ const certificateGenerator = require('./certificategenerator.js');
 const routes = require("./routes.js");
 const fastify = require('fastify')
 const decorators = require("./decorators/decorators.js");
+const zlib = require('zlib');
 
 class FastifyServer {
-    constructor(){
+    constructor() {
         this.server = fastify({
             logger: true,
             http2: true,
@@ -15,13 +16,29 @@ class FastifyServer {
                 cert: certificateGenerator.CERT
             }
         });
+
+        // Is this shit even correct? IDK send help //
+        this.server.addContentTypeParser('application/json', { parseAs: 'buffer' }, function (req, body, done) {
+            try {
+                if (req.body != null) {
+                    var json = JSON.parse(body)
+                    done(null, json)
+                } else {
+                    done(null, null);
+                }
+            } catch (err) {
+                err.statusCode = 400
+                done(err, undefined)
+            }
+        })
+
         //this.server.register(uWebSocket);
         //this.setWebSocketServer();
         this.defaultDecorators();
         this.server.register(
             require('fastify-compress'),
-            { 
-                encodings: ['deflate'],
+            {
+                encodings: ['deflate', 'gzip'],
                 global: true,
                 threshold: 0,
             });
@@ -29,22 +46,23 @@ class FastifyServer {
             secret: "nigga", // for cookies signature
             parseOptions: {}     // options for parsing cookies
         });
+
         routes.initializeRoutes(this.server);
     }
-    
-    async addRequestDecorator(FunctionName, Function){
+
+    async addRequestDecorator(FunctionName, Function) {
         this.server.decorateRequest(FunctionName, Function);
     }
 
-    async addResponseDecorator(FunctionName, Function){
+    async addResponseDecorator(FunctionName, Function) {
         this.server.decorateReply(FunctionName, Function);
     }
 
-    async addGlobalDecorator(FunctionName, Function){
+    async addGlobalDecorator(FunctionName, Function) {
         this.server.decorate(FunctionName, Function);
     }
 
-    async defaultDecorators(){
+    async defaultDecorators() {
         this.addRequestDecorator('reqBody2Json', decorators.bodyToJson);
         this.addRequestDecorator('reqDecompress', decorators.fastifyDecompress);
         this.addResponseDecorator('responseHeader', decorators.headerFormat);
@@ -52,12 +70,10 @@ class FastifyServer {
         this.addResponseDecorator('resNoBody', decorators.noBodyFormat);
     }
 
-    async startServer(){
-        try
-        {
+    async startServer() {
+        try {
             await this.server.listen(443, '127.0.0.1');
-        } catch(error)
-        {
+        } catch (error) {
             console.log(error);
         }
     }
